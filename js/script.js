479763e14623720488001166e42861c4ca06172b
Keda37 <fakesupp@gmail.com>
2018-08-11 23:53:26 +0300

    add video

--- js/script.js
+++ js/script.js
@@ -10,12 +10,60 @@ $(function() {
   });
 
   // загрузка видео с ютуба
-  $('.video__inner').click(function() {
-    $(this).append(
-      '<iframe width="642" height="401" src="https://www.youtube.com/embed/yrXRTCMGkZw?enablejsapi=1&amp;autoplay=1" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen style="display:none"></iframe>'
-    );
-    $('.video__inner iframe').fadeIn(500, function() {
-      $('.video__overlay').fadeOut();
+  $('.video-add').click(function(e) {
+    e.preventDefault();
+    if ($('.modal__iframe').length > 0) {
+      $('.modal')
+        .addClass('active')
+        .hide()
+        .fadeIn(300, function() {
+          $('.modal__iframe').each(function() {
+            $(this)[0].contentWindow.postMessage(
+              '{"event":"command","func":"playVideo","args":""}',
+              '*'
+            );
+          });
+        });
+    } else {
+      $('.modal')
+        .addClass('active')
+        .hide()
+        .fadeIn(300);
+      $('.modal__body').html(
+        '<iframe class="modal__iframe" src="https://www.youtube.com/embed/egjzHb9Wsh4?enablejsapi=1&amp;autoplay=1" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen"></iframe>'
+      );
+    }
+  });
+
+  $('.modal__close').click(function() {
+    $('.modal__iframe').each(function() {
+      $(this)[0].contentWindow.postMessage(
+        '{"event":"command","func":"pauseVideo","args":""}',
+        '*'
+      );
     });
+    $('.modal').fadeOut(300, function() {
+      $('.modal').removeClass('active');
+    });
+  });
+  $(document).mouseup(function(e) {
+    // событие клика по веб-документу
+    var div = $('.modal__inner'); // тут указываем элемента
+    if (
+      !div.is(e.target) &&
+      div.has(e.target).length === 0 &&
+      div.is(':visible')
+    ) {
+      // и не по его дочерним элементам
+      $('.modal__iframe').each(function() {
+        $(this)[0].contentWindow.postMessage(
+          '{"event":"command","func":"pauseVideo","args":""}',
+          '*'
+        );
+      });
+      $('.modal').fadeOut(300, function() {
+        $('.modal').removeClass('active');
+      });
+    }
   });
 });
